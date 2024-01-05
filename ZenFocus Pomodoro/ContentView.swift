45c9969129f9d7559744c63f5a024016d4176c3a
kwawannan <kwaw.annan@telesair.com>
2024-01-04 22:57:17 -0800

    Initial Commit

---
+++ ZenFocus Pomodoro/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ZenFocus Pomodoro
+//
+//  Created by Kwaw Annan on 1/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ZenFocus Pomodoro/ZenFocus_PomodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZenFocus_PomodoroApp.swift
+//  ZenFocus Pomodoro
+//
+//  Created by Kwaw Annan on 1/4/24.
+//
+
+import SwiftUI
+
+@main
+struct ZenFocus_PomodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
