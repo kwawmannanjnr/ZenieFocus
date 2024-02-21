6173427ca4ded682fb792fde614e9f006dfb483f
Kwaw Annan <kwawmannanjnr@gmail.com>
2024-02-20 16:10:41 -0800

    implemented  Foreground/Background App States detection

--- ZenFocus/ZenFocus/ZenFocusApp.swift
+++ ZenFocus/ZenFocus/ZenFocusApp.swift
@@ -9,9 +9,25 @@ import SwiftUI
 
 @main
 struct ZenFocusApp: App {
+    @Environment(\.scenePhase) private var scenePhase
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .onChange(of: scenePhase) { newScenePhase in
+                    switch newScenePhase {
+                    case .background:
+                        print("App moved to background")
+                        // Stop or pause Pomodoro timer
+                    case .active:
+                        print("App is in foreground")
+                        // Conditionally resume or start new Pomodoro timer
+                    case .inactive:
+                        break
+                    @unknown default:
+                        print("Unexpected new scene phase.")
+                    }
+                }
         }
     }
 }
